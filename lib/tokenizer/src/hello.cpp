d86118713bfbd688ba9b93f00b7ba7b538b45c7a
GoscZnickiem <maciek.6098@gmail.com>
2026-03-15 23:33:20 +0100

    cleanup

--- lib/tokenizer/src/hello.cpp
+++
@@ -1,9 +0,0 @@
-#include "tokenizer/hello.hpp"
-#include <iostream>
-
-int hello() {
-	std::cout << ":3\n";
-
-	constexpr auto ret = 42;
-	return ret;
-}
--- lib/tokenizer/test/test_hello.cpp
+++
@@ -1,9 +0,0 @@
-#include <gtest/gtest.h>
-#include "tokenizer/hello.hpp"
-
-TEST(hello, world) {
-	const auto r = hello();
-	constexpr int exp = 42;
-
-	EXPECT_TRUE(r == exp);
-}
